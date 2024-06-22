52786a69c56f6a1d1979ee99092a106f8ec87fba
JudasM <judasmohlala2012@gmail.com>
2024-06-22 13:23:08 +0200

    changed the search page to design ya Mr Paseka.

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
 session_start();
 include "db_conn.php";
 
-// Retrieve the first name and last name from the URL if they are set and add to the session
+// Retrieve the first name and last name from the URL if they are set add to the session
 $first_name = isset($_GET['first_name']) ? htmlspecialchars($_GET['first_name']) : '';
 $last_name = isset($_GET['last_name']) ? htmlspecialchars($_GET['last_name']) : '';
 $candidate_ID = isset($_GET['candidate_ID']) ? htmlspecialchars($_GET['candidate_ID']) : '';
@@ -15,6 +15,30 @@ $_SESSION['candidate_ID'] = $candidate_ID; // Add the application id into the se
 // Concatenate first name and last name with a space between them
 $full_name = $first_name . ' ' . $last_name;
 $_SESSION['full_name'] = $full_name;
+
+
+// Count applicants based on their status
+$accepted_count = 0;
+$declined_count = 0;
+$in_progress_count = 0;
+$completed_count = 0;
+
+$sql_count = "SELECT status_id, COUNT(*) as count FROM applicant GROUP BY status_id";
+$result_count = mysqli_query($conn, $sql_count);
+while ($row_count = mysqli_fetch_assoc($result_count)) {
+    if ($row_count['status_id'] == 'accepted') {
+        $accepted_count = $row_count['count'];
+    } elseif ($row_count['status_id'] == 'declined') {
+        $declined_count = $row_count['count'];
+    } elseif ($row_count['status_id'] == 'in_progress') {
+        $in_progress_count = $row_count['count'];
+    } elseif ($row_count['status_id'] == 'completed') {
+        $completed_count = $row_count['count'];
+    }
+    
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,24 +50,46 @@ $_SESSION['full_name'] = $full_name;
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmF/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="assets/css/searchpage.css">
     <style>
+        .status-circle {
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            color: white;
+            font-size: 1rem;
+            margin-right: 10px;
+        }
+        .accepted {
+            background-color: green;
+        }
+        .declined {
+            background-color: red;
+        }
+        .in-progress {
+            background-color: orange;
+        }
+        .completed {
+            background-color: blue;
+        }
+
+        
         .table-wrapper {
             max-height: 400px;
             overflow-y: auto;
         }
-
         .table-hover tbody tr:hover {
             cursor: pointer;
             background-color: #f5f5f5;
         }
-
         .btn-vetting-default {
             background-color: grey;
             border-color: grey;
         }
-
         .btn-vetting-clicked {
             background-color: blue;
             border-color: blue;
@@ -96,36 +142,65 @@ $_SESSION['full_name'] = $full_name;
         </div>
         <div class="d-flex align-items-right">
             <?php
-             if (isset($_SESSION['username'])) {
+            if (isset($_SESSION['username'])) {
                 // Display an active dot and the username
                 echo '<span><i class="fas fa-circle text-success me-1"></i>' . $_SESSION['username'] . '</span>';
-              }
+            }
             ?>
         </div>
     </div>
 
     <div class="container">
-        <div class="row height d-flex justify-content-center align-items-center">
-            <div class="col-md-8">
-                <form id="searchForm" action="vetting.php" method="POST">
-                    <div class="search">
-                        <i class="fa fa-search"></i>
-                        <input type="text" name="searchKey" id="searchKey" class="form-control" placeholder="View applicant table (Menu) or enter full name of a person to search" value="<?php echo $full_name; ?>">
-                        <button class="btn btn-primary">Search</button>
-                    </div>
-                    <div class="mt-2">
-                        <label for="linkedin" class="form-label">Please choose the social media platforms you'd like to search:</label><br>
-                        <input type="checkbox" id="linkedin" name="linkedin" value="linkedin">
-                        <label for="linkedin">LinkedIn</label><br>
-                        <input type="checkbox" id="facebook" name="facebook" value="facebook">
-                        <label for="facebook">Facebook</label><br>
-                        <input type="checkbox" id="instagram" name="instagram" value="instagram">
-                        <label for="instagram">Instagram</label><br>
-                        <input type="checkbox" id="twitter" name="twitter" value="twitter">
-                        <label for="twitter">Twitter</label>
-                    </div>
-                </form>
+   <!-- <div class="row height d-flex justify-content-center align-items-center">  -->
+   <div class="row d-flex justify-content-center align-items-center">
+    <div class="col-md-8">
+        <form action="result.php" method="POST">
+            <div class="search">
+                <i class="fa fa-search"></i>
+                <input type="text" name="searchKey" id="searchKey" class="form-control" placeholder="View applicant table (Menu) or enter full name of a person to search" value="<?php echo $full_name; ?>">
+                <button class="btn btn-primary">Search</button>
             </div>
+            
+            <div class="mt-2 mb-4">
+                <label for="socialMedia" class="form-label">Please choose the social media platforms you'd like to search:</label><br>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="linkedin" name="linkedin" value="linkedin">
+                    <label class="form-check-label" for="linkedin">LinkedIn</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="facebook" name="facebook" value="facebook">
+                    <label class="form-check-label" for="facebook">Facebook</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="instagram" name="instagram" value="instagram">
+                    <label class="form-check-label" for="instagram">Instagram</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="twitter" name="twitter" value="twitter">
+                    <label class="form-check-label" for="twitter">Twitter</label>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="status-circle accepted"><?php echo  $accepted_count; ?></div> Accepted
+            </div>
+            <div class="col">
+                <div class="status-circle declined"><?php  echo $declined_count; ?></div> Declined
+            </div>
+            <div class="col">
+                <div class="status-circle in-progress"><?php echo $in_progress_count; ?></div> In Progress
+            </div>
+            <div class="col">
+                <div class="status-circle completed"><?php  echo $completed_count; ?></div> Completed
+            </div>
+        </div>
+        <br>
+
             <!-- Add the Table for applicants -->
             <div class="table-wrapper mt-2">
                 <table class="table table-hover text-center" id="applicantTable">
@@ -171,7 +246,7 @@ $_SESSION['full_name'] = $full_name;
                                 <td>
                                     <a href="view_applicant.php?identity_number=<?php echo $row["candidate_ID"] ?>" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i> Vetting</a>
                                     <a href="search.php?first_name=<?php echo $row["first_name"]; ?>&last_name=<?php echo $row["last_name"]; ?>&candidate_ID=<?php echo $row["candidate_ID"]; ?>" class="btn btn-info btn-sm"><i class="fas fa-search"></i> Search</a>
-                               </td>
+                                </td>
                             </tr>
                         <?php
                         }
